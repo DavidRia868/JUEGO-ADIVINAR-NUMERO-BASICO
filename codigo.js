0baa9d1049ee1d7bf3566d5d13e0d074b7b5e5cb
edriascos888 <davoriascos888@hotmail.com>
2024-06-05 20:16:21 -0500

    Primer Commit Subiendo Proyecto

---
+++ codigo.js
@@ -0,0 +1,44 @@
+
+
+document.getElementById("BTN").addEventListener("click", function() {
+
+let numeroUsuario = document.getElementById('num').value;
+
+// convertir
+
+let numUsuario1 = Number(numeroUsuario);
+let numAleatorio = Math.round(Math.random()*10);
+
+//let boton = document.getElementById('BTN');
+//boton.addEventListener('click',ver())
+    
+    if(isNaN(numUsuario1))
+        {
+            document.getElementById('mensaje').innerText = "NO Es un número"
+            
+        }
+        else if(numUsuario1> 10){
+            document.getElementById('mensaje').innerText = "Error - El Número es mayor a 10"
+        }
+        else if (numAleatorio != numUsuario1)
+        {
+            if(numUsuario1> numAleatorio){
+                document.getElementById('mensaje').innerText = " El número digitado es mayor";
+            }
+            else{
+                document.getElementById('mensaje').innerText = " El número digitado es menor";
+            }
+            
+        }
+        
+        else{
+            document.getElementById('mensaje').innerText = "😀 FELICIDADES - ADIVINASTE EL NÚMERO 😀"
+        }
+});
+
+function borrar(){
+
+    numeroUsuario = document.getElementById('num').value = "";
+    document.getElementById('mensaje').innerText = "";
+    
+}
